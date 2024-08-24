69ca6ecab00e2a30612afd7aa2a61e730a026bcf
DurgeshNandmehar <ADRaftar369@gmail.com>
2024-08-24 15:34:43 +0530

    update

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,14 +1,23 @@
-document.getElementById("hamButton").addEventListener('click', function () {
-    document.getElementById("hamButton").classList.toggle("hamMenuClose");
-})
+document.getElementById("hamButton").addEventListener("click", function () {
+  document.getElementById("hamButton").classList.toggle("hamMenuClose");
+});
 
 // navbar Collaps
 let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
 
 navBar.forEach(function (a) {
-    a.addEventListener("click", function () {
-        navCollapse.classList.remove('show');
-        document.getElementById("hamButton").classList.toggle("hamMenuClose");
-    })
-})
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+    document.getElementById("hamButton").classList.toggle("hamMenuClose");
+  });
+});
+
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach((link) =>
+  link.addEventListener("click", function () {
+    links.forEach((link) => link.classList.remove("active"));
+    this.classList.add("active");
+  })
+);
